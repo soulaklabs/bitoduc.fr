ac6b213ca16dccaab6371a2341c4e81426b0ae77
Valentin Lorentz <progval@progval.net>
2015-12-09 12:58:33 +0100

    Traduction des noms de fonction en français.

---
+++ fr.js
@@ -0,0 +1,26 @@
+Objet = Object
+//Objet.prototype.ajouter = Objet.prototype.append
+Objet.creer = Objet.create
+
+Chaine = String
+Chaine.prototype.enMinuscules = Chaine.prototype.toLowerCase
+Chaine.prototype.enMajuscules = Chaine.prototype.toUpperCase
+Chaine.prototype.remplacer = Chaine.prototype.replace
+Chaine.prototype.caractereA = Chaine.prototype.charAt
+
+Tableau = Array
+Tableau.prototype.tri = Tableau.prototype.sort
+Tableau.prototype.longueur = function () { return this.length; }
+Tableau.prototype.pousser = Tableau.prototype.push
+
+document.creerElement = document.createElement
+document.recupererElementParNomDEtiquette = document.getElementsByTagName;
+document.localisation = document.location;
+document.localisation.protocole = document.localisation.protocole;
+
+jQuery.fn.extend({
+    enfants: function () { return this.children(); },
+    dernier: function () { return this.last(); },
+    ajouter: function (x) { return this.append(x); }
+});
+$.recupererJSON = $.getJSON
